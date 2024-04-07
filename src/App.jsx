c39db47d58a8d348cbc72375d6f5f8bec779d27d
VinothaArivalagan <vinothag96@gmail.com>
2024-04-07 15:28:11 +0200

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import LikeButton from "./components/LikeButton";
+import Counter from "./components/counter";
+import ClickablePicture from "./components/ClickablePicture";
+import Dice from "./components/Dice";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <br />
+      <h2> Like Button</h2>
+      <LikeButton />
+      <h2>Iteration 2 | State: Counter</h2>
+      <Counter />
+      <h3> Clickable Picture</h3>
+      <ClickablePicture /> 
+      <h2> Dice</h2>
+      <Dice />
     </div>
   );
 }
